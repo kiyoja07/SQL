1cce187d556ca89466f11d361c96de29e4dcfe91
kiyoja07 <kiyoja07@gmail.com>
2019-12-09 12:33:04 +0900

    add script of bigquery

--- bun/bigquery.sql
+++ bun/bigquery.sql
@@ -17,3 +17,19 @@ where event_name = 'select_content'
   and (event_param.key = 'item_id' and event_param.value.string_value ='연락하기') 
 group by event_date, event_param.key, event_param.value.string_value
 order by event_date
+
+
+
+select event_date, 
+  (select event_param.value.string_value
+  from UNNEST(event_params) as event_param
+  where event_param.key = 'content_type') as content_type,
+  (select event_param.value.string_value
+  from UNNEST(event_params) as event_param
+  where event_param.key = 'source') as source,
+  count(event_timestamp) as count
+from `bunjang-68108.analytics_153554412.events_*`
+corss join UNNEST(event_params) as event_param
+where _TABLE_SUFFIX BETWEEN '20191101' and '20191130'
+  and event_name = 'request_register_item'
+group by 1, 2, 3  
